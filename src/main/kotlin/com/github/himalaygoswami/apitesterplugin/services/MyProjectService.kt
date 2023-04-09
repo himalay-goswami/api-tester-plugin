4f0906027570edd1f3ec128cf2321dfbe574eb1a
GitHub Action <action@github.com>
2023-04-09 14:54:34 +0000

    Template cleanup

--- src/main/kotlin/com/github/himalaygoswami/apitesterplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/himalaygoswami/apitesterplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.himalaygoswami.apitesterplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.himalaygoswami.apitesterplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
